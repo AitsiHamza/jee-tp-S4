80dcbab00e008a65274c4eff0cbcc0d62d72cbf6
AITSIHAMZA <hamzaaitsi65@gmail.com>
2022-03-12 14:08:33 +0100

    1st code

---
+++ jpa-ap/src/main/java/ma/enset/jpaap/JpaApApplication.java
@@ -0,0 +1,32 @@
+package ma.enset.jpaap;
+
+import ma.enset.jpaap.entities.Patient;
+import ma.enset.jpaap.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootApplication
+public class JpaApApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null,"fatima",new Date(),false,56));
+        patientRepository.save(new Patient(null,"brahim",new Date(),false,56));
+        patientRepository.save(new Patient(null,"meriem",new Date(),false,56));
+        List<Patient> patients = patientRepository.findAll();
+        patients.forEach(p->{
+            System.out.println(p.getId());
+        });
+    }
+}
