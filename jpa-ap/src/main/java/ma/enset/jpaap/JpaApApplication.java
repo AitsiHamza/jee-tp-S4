8bf5020b72901794d135fab26322305b0a8e6764
AITSIHAMZA <hamzaaitsi65@gmail.com>
2022-03-12 16:27:51 +0100

    8nd change : predifind methods in springData:

--- jpa-ap/src/main/java/ma/enset/jpaap/JpaApApplication.java
+++ jpa-ap/src/main/java/ma/enset/jpaap/JpaApApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -23,12 +24,12 @@ public class JpaApApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Date d1=new Date();
         for (int i = 0; i < 100; i++) {
             patientRepository.save(new Patient(null, "azer", new Date(), (Math.random()>0.5)?true:false, (int)(Math.random()*100)));
         }
-        /**find all isn't always recommended*/
-        /**we can use the pagination*/
-        List<Patient> patients = patientRepository.findBySickIsTrueAndScoreLessThan(50);
+        Date d2=new Date();
+        List<Patient> patients = patientRepository.findByBirthDayBetween(d1,d2);
         patients.forEach(p->{
             System.out.println("==============================");
             System.out.println(p.getId());
--- jpa-ap/src/main/java/ma/enset/jpaap/repositories/PatientRepository.java
+++ jpa-ap/src/main/java/ma/enset/jpaap/repositories/PatientRepository.java
@@ -3,8 +3,9 @@ package ma.enset.jpaap.repositories;
 import ma.enset.jpaap.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
-    List<Patient> findBySickIsTrueAndScoreLessThan(int score);
+    List<Patient> findByBirthDayBetween(Date d1,Date d2);
 }
