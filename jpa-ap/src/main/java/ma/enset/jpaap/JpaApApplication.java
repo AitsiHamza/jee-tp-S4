93b80359d3e300aed6596aaa5243bc6e7042c347
AITSIHAMZA <hamzaaitsi65@gmail.com>
2022-03-12 14:31:33 +0100

    6nd change : pagination

--- jpa-ap/src/main/java/ma/enset/jpaap/JpaApApplication.java
+++ jpa-ap/src/main/java/ma/enset/jpaap/JpaApApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.Date;
 import java.util.List;
@@ -21,11 +23,17 @@ public class JpaApApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        patientRepository.save(new Patient(null,"fatima",new Date(),false,56));
-        patientRepository.save(new Patient(null,"brahim",new Date(),false,56));
-        patientRepository.save(new Patient(null,"meriem",new Date(),false,56));
-        List<Patient> patients = patientRepository.findAll();
-
-        patientRepository.deleteById(1L);
+        for (int i = 0; i < 100; i++) {
+            patientRepository.save(new Patient(null, "azer", new Date(), (Math.random()>0.5)?true:false, 56));
+        }
+        /**find all isn't always recommended*/
+        /**we can use the pagination*/
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(0,5));
+        patients.forEach(p->{
+            System.out.println("==============================");
+            System.out.println(p.getId());
+            System.out.println(p.getName());
+            System.out.println(p.isSick());
+        });
     }
 }
